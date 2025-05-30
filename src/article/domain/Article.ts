17fa339fbadf80c91c1c43f757259f417a9c852b
kitechoi <chevel0212@gmail.com>
2025-05-30 17:49:52 +0900

    [style]: Article 제목 길이

--- src/article/domain/Article.ts
+++ src/article/domain/Article.ts
@@ -1,14 +1,11 @@
-import { Password } from '../../user/domain/Password';
 import { Result } from '../../shared/core/domain/Result';
 import { AggregateRoot } from 'src/shared/core/domain/AggregateRoot';
 import { UniqueEntityID } from 'src/shared/core/domain/UniqueEntityID';
-import { User } from 'src/user/domain/User';
 
 export interface ArticleProps {
   title: string;
   content: string;
-  // authorId: string; // 유저 username
-  userId: number; // 유저 pk
+  userId: number;
 }
 
 export class Article extends AggregateRoot<ArticleProps> {
@@ -17,7 +14,7 @@ export class Article extends AggregateRoot<ArticleProps> {
   }
 
   static create(props: ArticleProps, id?: UniqueEntityID): Result<Article> {
-    if (!props.title || props.title.length > 5) {
+    if (!props.title || props.title.length > 50) {
       return Result.fail('제목은 50자 이하로 입력해야 합니다.');
     }
 
@@ -25,7 +22,7 @@ export class Article extends AggregateRoot<ArticleProps> {
       return Result.fail('본문은 2000자 이하로 입력해야 합니다.');
     }
 
-    if (!props.userId ) {
+    if (!props.userId) {
       return Result.fail('작성자가 확인돼야 합니다.');
     }
 
