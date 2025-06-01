2fc8e712b0ab782119f359ed211d4c27685e3e9a
kitechoi <chevel0212@gmail.com>
2025-06-01 13:57:43 +0900

    feat: 게시글 도메인 및 엔티티 구현

---
+++ src/article/infrastructure/entity/ArticleEntity.ts
@@ -0,0 +1,36 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { UserEntitiy } from 'src/user/infrastructure/entity/UserEntity';
+
+@Entity('articles')
+export class ArticleEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @ManyToOne(() => UserEntitiy)
+  @JoinColumn({ name: 'user_id' })
+  user: UserEntitiy;
+
+  // @Column()
+  // user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  update_at: Date;
+}
