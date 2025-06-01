06d72998054b93e4f6ce194eb959c90717993f43
kitechoi <chevel0212@gmail.com>
2025-06-01 14:24:04 +0900

    feat: HTTP 요청/응답 로깅 인터셉터 구현

---
+++ src/shared/interceptors/HttpRequestLogginInterceptor.ts
@@ -0,0 +1,29 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { HttpLogger } from '../log/HttpLogger';
+
+@Injectable()
+export class HttpRequestLoggingInterceptor implements NestInterceptor {
+  async intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Promise<Observable<unknown>> {
+    const request = context.switchToHttp().getRequest();
+
+    const logger = new HttpLogger('request');
+    await logger.log({
+      context: 'request',
+      url: request.originalUrl,
+      method: request.method,
+      body: request.body,
+      // headers: request.headers,
+    });
+
+    return next.handle();
+  }
+}
