e3cf62f0e462fdb83674af95ebcbab4229e1ba28
kitechoi <chevel0212@gmail.com>
2025-06-01 13:57:43 +0900

    feat: 게시글 생성(CreateArticle) 유스케이스 및 컨트롤러 구현

---
+++ src/article/presentation/dto/ArticleControllerResponse.ts
@@ -0,0 +1,19 @@
+export class ArticleControllerCreateArticleResponse {
+  id: number;
+  title: string;
+  content: string;
+  username: string;
+}
+
+export class ArticleControllerUpdateArticleResponse {
+  id: number;
+  title: string;
+  content: string;
+  username: string;
+}
+
+export class ArticleControllerFineAllArticleResponse {
+  id: number;
+  title: string;
+  content: string;
+}
